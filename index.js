bf0ce3343f47c1b3721f81ccc11738ffca038446
darkhan.jumabekov@gmail.com <darkhan.jumabekov@gmail.com>
2022-10-23 00:15:22 +0600

    update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import {handleValidationErrors, checkAuth} from './utils/index.js'
 
 
 mongoose.connect(
-    'mongodb+srv://admin:123456zZ!@cluster0.me6uhrn.mongodb.net/blog?retryWrites=true&w=majority'
+    process.env.MONGODB_URI,
     )
     .then(()=>console.log('DB ok'))
     .catch((err)=>console.log('DB error', err));
@@ -52,7 +52,7 @@ app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, Post
 app.delete('/posts/:id', checkAuth, PostController.remove);
 app.patch('/posts/:id',  checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
 
-app.listen(4444, (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
     if(err){
         return console.log(err);
     } else{
