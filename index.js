0391afca2fe8d4cb1eae95e62af429768ae6eea7
darkhan.jumabekov@gmail.com <darkhan.jumabekov@gmail.com>
2022-10-28 15:59:14 +0600

    add serve

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import multer from 'multer';
-// import cors from 'cors'
+import cors from 'cors';
 import mongoose from 'mongoose';
 import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
 import { PostController, UserController } from './controllers/index.js';
@@ -26,7 +26,7 @@ const upload = multer({ storage });
 
 app.use(express.json());
 
-// app.use(cors());
+app.use(cors());
 
 app.use('/uploads', express.static('uploads'));
 
